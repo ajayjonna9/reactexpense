2ba9212fc473156af8cd728a7e555c096f0dfca3
AJ <jonnaajay999@gmail.com>
2022-11-22 17:10:09 +0530

    use state to form

--- src/Components/expanseForm/expanseForm.js
+++ src/Components/expanseForm/expanseForm.js
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
 import "./ExpanseForm.css";
-
 const ExpanseForm = () => {
-  const onChangeInput = (e) => {
-    console.log(e.target.value);
-  };
+  const [expeseTitle, setExpeseTitle] = useState("");
+  const [expanseAmount, setExpanseAmount] = useState("");
+  const [date, setdate] = useState("");
 
+  const onChangeInput = (e) => {
+    e.preventDefault();
+    if (e.target.name === "expanseTitle") {
+      setExpeseTitle(e.target.value);
+    } else if (e.target.name === "expanseAmount") {
+      setExpanseAmount(e.target.value);
+    } else {
+      setdate(e.target.value);
+    }
+  };
   return (
     <div className="form">
       <form>
         <h5>Expanse Titile</h5>
         <input
           type="text"
-          name="expanseTitile"
+          name="expanseTitle"
+          value={expeseTitle}
           onChange={onChangeInput}
         ></input>
         <h5>Expanse Amount</h5>
         <input
           type="number"
           name="expanseAmount"
+          value={expanseAmount}
           onChange={onChangeInput}
         ></input>
         <h5>Date</h5>
-        <input type="date" name="date" onChange={onChangeInput}></input>
+        <input
+          type="date"
+          name="date"
+          value={date}
+          onChange={onChangeInput}
+        ></input>
         <button>submit</button>
       </form>
     </div>
