e74036d08a72a3fe09e22caac2145dad4769c044
AJ <jonnaajay999@gmail.com>
2022-11-22 18:15:56 +0530

    createobject

--- src/Components/expanseForm/expanseForm.js
+++ src/Components/expanseForm/expanseForm.js
@@ -1,45 +1,74 @@
 import React, { useState } from "react";
 import "./ExpanseForm.css";
 const ExpanseForm = () => {
-  const [expeseTitle, setExpeseTitle] = useState("");
-  const [expanseAmount, setExpanseAmount] = useState("");
-  const [date, setdate] = useState("");
+  //   const [expeseTitle, setExpeseTitle] = useState("");
+  //   const [expanseAmount, setExpanseAmount] = useState("");
+  //   const [date, setdate] = useState("");
+  const [inputValues, setInputValues] = useState({
+    expeseTitle: "",
+    expanseAmount: "",
+    date: "",
+  });
 
   const onChangeInput = (e) => {
     e.preventDefault();
     if (e.target.name === "expanseTitle") {
-      setExpeseTitle(e.target.value);
+      //   setExpeseTitle(e.target.value);
+      // setInputValues({...inputValues,expeseTitle:e.target.value})
+      setInputValues((prevals) => {
+        return {
+          ...prevals,
+          expeseTitle: e.target.value,
+        };
+      });
     } else if (e.target.name === "expanseAmount") {
-      setExpanseAmount(e.target.value);
+      setInputValues((prevals) => {
+        return {
+          ...prevals,
+          expanseAmount: e.target.value,
+        };
+      });
     } else {
-      setdate(e.target.value);
+      setInputValues((prevals) => {
+        return {
+          ...prevals,
+          date: e.target.value,
+        };
+      });
     }
+  };
+  const onsubmit = (e) => {
+    e.preventDefault();
+    const obj = {
+      ...inputValues,
+    };
+    console.log(obj);
   };
   return (
     <div className="form">
-      <form>
+      <form onSubmit={onsubmit}>
         <h5>Expanse Titile</h5>
         <input
           type="text"
           name="expanseTitle"
-          value={expeseTitle}
+          //value={expeseTitle}
           onChange={onChangeInput}
         ></input>
         <h5>Expanse Amount</h5>
         <input
           type="number"
           name="expanseAmount"
-          value={expanseAmount}
+          //value={expanseAmount}
           onChange={onChangeInput}
         ></input>
         <h5>Date</h5>
         <input
           type="date"
           name="date"
-          value={date}
+          //value={date}
           onChange={onChangeInput}
         ></input>
-        <button>submit</button>
+        <button type="submit">submit</button>
       </form>
     </div>
   );
